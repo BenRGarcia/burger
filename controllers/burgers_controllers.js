297b4eedefa173bb12fc6a7b95466ab100787bdc
Ben Garcia <ben@seebenprogram.com>
2018-04-15 19:50:51 -0400

    Build framework of express router

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,19 @@
+// Require dependency
+const express = require('express');
+const burger = require('../models/burger.js');
+
+// Create router
+const apiRouter = express.Router();
+
+// Define methods for PATH '/'
+apiRouter.route('/')
+  .get((req, res, next) => {})
+  .post((req, res, next) => {})
+  .put((req, res, next) => {});
+
+// Client-Side Error handling
+apiRouter.use((err, req, res, next) => {
+  res.status(400).send({ error: `Internal Server Error` });
+});
+
+module.exports = apiRouter;
