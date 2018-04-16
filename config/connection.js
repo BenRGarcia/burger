0c56eec73ea3c2e2ae2e77e3b4c5a3d5291dd086
Ben Garcia <ben@seebenprogram.com>
2018-04-15 20:35:02 -0400

    Add config for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,21 @@
 require('dotenv').config()
 // Database dependency
 const mysql = require('mysql');
-// Set database connection's .env credentials
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
 
+// 
+if (process.env.JAWSDB_URL) {
+  // Set database connection's deployed .env credentials
+  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // Set database connection's local .env credentials
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+}
+
+connection.connect();
 module.exports = connection;
