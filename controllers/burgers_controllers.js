ab21edd2a5e3b09efa84cc3a784060ece6103c98
Ben Garcia <ben@seebenprogram.com>
2018-04-16 14:05:34 -0400

    Add wildcard redirect

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -5,11 +5,15 @@ const burger = require('../models/burger.js');
 // Create router
 const router = express.Router();
 
-// Define methods for PATH '/api/burgers'
+// Define methods for PATH '/'
 router.route('/')
   .get((req, res, next) => { res.render('index');})
   .post((req, res, next) => {})
   .put((req, res, next) => {});
+
+router.get('*', (req, res, next) => {
+  res.redirect(301, '/');
+});
 
 // Client-Side Error handling
 router.use((err, req, res, next) => {
