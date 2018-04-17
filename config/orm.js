e62c8f2950cd4eb9c25b0cbd78b8bb89d72ffc24
Ben Garcia <ben@seebenprogram.com>
2018-04-17 11:03:31 -0400

    Refactor DB queries to call 'executeQuery'

--- config/orm.js
+++ config/orm.js
@@ -3,38 +3,55 @@ const mysql = require('mysql');
 // Establish connection to database
 const connection = require('./connection.js');
 
+// Function that actually executes DB query
+const executeQuery = (query, params = null) => {
+  return new Promise((resolve, reject) => {
+    connection.query(query, params, (err, res, fields) => {
+      if (err) reject(err);
+      resolve(res);
+    });
+  });
+};
+
 const orm = {
-  // Get all burgers from database
+  // Get all burgers from database (READ)
   selectAll: () => {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM burgers', (err, res, fields) => {
-        if (err) reject(err);
-        resolve(res);
-      });
+      const query = 'SELECT * FROM burgers';
+      executeQuery(query)
+        .then(res => resolve(res))
+        .catch(err => reject(err));
     });
   },
-  // Add new burger to database
+  // Add new burger to database (CREATE)
   insertOne: ({ burger_name }) => {
     return new Promise((resolve, reject) => {
-      const queryString = 'INSERT INTO burgers SET ?';
-      connection.query(queryString, { burger_name }, (err, res, fields) => {
-        if (err) reject(err);
-        resolve(res);
-      });
+      const query = 'INSERT INTO burgers SET ?';
+      const params = { burger_name };
+      executeQuery(query, params)
+        .then(res => resolve(res))
+        .catch(err => reject(err));
     });
   },
-  // Change burger from 'devoured: 0' to 'devoured: 1'
+  // Change burger 'devoured' 0 -> 1 (UPDATE)
   updateOne: ({ burger_name }) => {
     return new Promise((resolve, reject) => {
-      const queryString = 'UPDATE burgers SET ? WHERE ?';
-      connection.query(queryString, [{ devoured: 1}, { burger_name }], (err, res, fields) => {
-        if (err) reject(err);
-        resolve(res);
-      });
+      const query = 'UPDATE burgers SET ? WHERE ?';
+      const params = [{ devoured: true },{ burger_name }];
+      executeQuery(query, params)
+        .then(res => resolve(res))
+        .catch(err => reject(err));
     });
   },
-  // Delete all burgers from the database
-  deleteAll: () => {}
+  // Delete all burgers from the database (DELETE)
+  deleteAll: () => {
+    return new Promise((resolve, reject) => {
+      const query = 'TRUNCATE TABLE burgers';
+      executeQuery(query)
+        .then(res => resolve(res))
+        .catch(err => reject(err));
+    });
+  }
 };
 
 module.exports = orm;
